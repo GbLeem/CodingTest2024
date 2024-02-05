6cbc4872b4c18349c640413c1e55162a72807631
GB_LEEM <juntrex@naver.com>
2024-02-05 16:19:20 +0900

    [TEMP]

--- GameAlgorithm/BFS_DFS/baek2178.cpp
+++ GameAlgorithm/BFS_DFS/baek2178.cpp
@@ -1,4 +1,4 @@
-//¹Ì·Î Å½»ö - °Å¸® Ã£±â ¹®Á¦
+//ï¿½Ì·ï¿½ Å½ï¿½ï¿½ - ï¿½Å¸ï¿½ Ã£ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
 
 #include <iostream>
 #include <vector>
@@ -7,7 +7,6 @@
 using namespace std;
 
 string board[102];
-<<<<<<< HEAD
 int dist[102][102];
 int N, M;// ì„¸ë¡œ ê°€ë¡œ
 int dx[4] = { 1,0,-1,0 }; //í•˜ ìš° ìƒ ì¢Œ
@@ -50,57 +49,5 @@ int main()
 		}
 	}
 	cout << dist[N - 1][M - 1] + 1;
-=======
-int dist[502][502] = { -1, };
-int dx[4] = { 1, 0 ,-1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-int N, M;// ¼¼·Î °¡·Î
-
-//1ÀÌ ÀÌµ¿ÇÒ ¼ö ÀÖ´Â Ä­, (1,1) ¿¡¼­ ½ÃÀÛ
-
-int main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	cin >> N >> M; //4 6
-
-	for (int i = 0; i < N; ++i)
-	{
-		cin >> board[i];
-	}
-
-	for (int i = 0; i < N; ++i)
-	{
-		for (int j = 0; j < M; ++j)
-		{
-			dist[i][j] = -1;
-		}
-	}
-	queue<pair<int, int>> Q;
-	
-	dist[0][0] = 0; //¹æ¹®ÇÏ¸é °Å¸® 0À¸·Î ¿Ã·ÁÁÖ±â
-	Q.push({ 0, 0 });
-
-	while (!Q.empty())
-	{
-		pair<int, int> cur = Q.front();
-		Q.pop();
-
-		for (int dir = 0; dir < 4; ++dir)
-		{
-			int nx = cur.first + dx[dir];
-			int ny = cur.second + dy[dir];
-
-			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-				continue;
-			if (board[nx][ny] != '1' || dist[nx][ny] >= 0)
-				continue;
-
-			dist[nx][ny] = dist[cur.first][cur.second] + 1;
-			Q.push({ nx, ny });
-		}
-	}
-	cout << dist[N-1][M-1] + 1;
->>>>>>> 6f34ef2bbf800d67aca61e05bb1784918f1d5ff9
 }
+
