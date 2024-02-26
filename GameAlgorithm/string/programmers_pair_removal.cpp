3d01e2cab545520b613591e522bd2f6aa2498624
GB_LEEM <juntrex@naver.com>
2024-02-26 14:51:51 +0900

    [ADD] programmers level 2 pair removal

--- GameAlgorithm/string/programmers_pair_removal.cpp
+++ GameAlgorithm/string/programmers_pair_removal.cpp
@@ -1,45 +1,69 @@
 #include <iostream>
 #include<string>
 #include <iostream>
+#include <stack>
 using namespace std;
+
+//int solution(string s)
+//{
+//    int answer = -1;
+//    string str = s;
+//    int size = s.size();
+//
+//    while (1)
+//    {        
+//        if (str.size() == 1) //one letter left
+//            return 0;
+//        if (str.size() == 0) //clear all
+//            return 1;
+//
+//        s = str; //baabaa -> bbaa -> aa
+//        int index = 0;
+//
+//        if (s[index] == s[index + 1])
+//        {
+//            str.clear();
+//            str += s.substr(0, index + 1);
+//            str += s.substr(index + 2, s.size());
+//        }
+//        index++;
+//
+//        if (index >= size)
+//            return 0;
+//    }
+//}
 
 int solution(string s)
 {
-    int answer = -1;
-    string str = s;
-
-    while (1)
-    {
-        if (str.size() == 1)
-            return 0;
-        if (str.size() == 0)
-            return 1;
-
-        s = str;
-        str.clear();
-
-        for (size_t i = 0; i < s.size() - 1; ++i)
-        {
-            if ((int)s[i] == (int)s[i + 1])
-            {
-                //simple?                
-                for (size_t j = i + 2; j < s.size(); ++j)
-                {
-                    str += s[j];
-                }
-                break;
-            }
-            str += s[i];
-        }
-        //cout << str << " ";
-    }
+    stack<char> str;    
+    char temp = ' ';
     
-    return answer;
+    for (size_t i = 0; i < s.size(); ++i)
+    {
+        str.push(s[i]);
+
+        if (str.top() == temp)
+        {
+            str.pop();
+            str.pop();
+        }
+        if (!str.empty())
+            temp = str.top();
+        else
+            temp = ' ';
+    }
+
+    if (str.empty())
+        return 1;
+    else
+        return 0;
 }
 
-int main()
-{
-    cout << solution("baabaa") << endl;
-    cout << solution("cdcd") << endl;
 
+int main() 
+{
+    cout << solution("baabaa") << endl; //1
+    cout << solution("cdcd") << endl; //0
+    cout << solution("xx") << endl; //1
+    cout << solution("abbbaa") << endl; //0
 }
