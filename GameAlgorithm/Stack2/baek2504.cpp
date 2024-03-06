8338fb018d3f78bdb8f7c18e692b26026a013b65
GB_LEEM <juntrex@naver.com>
2024-03-06 11:26:44 +0900

    [ADD] barking dog - lecture stack2 (baek 2504)

--- GameAlgorithm/Stack2/baek2504.cpp
+++ GameAlgorithm/Stack2/baek2504.cpp
@@ -1,57 +1,4 @@
-//#include <iostream>
-//#include <stack>
-//#include <string>
-//using namespace std;
-//
-//int main()
-//{
-//	ios::sync_with_stdio(0);
-//	cin.tie(0);
-//
-//	string str;
-//	cin >> str;
-//	stack<char> st;
-//	int answer = 0;
-//
-//	for (int i = 0; i < str.size(); ++i)
-//	{
-//		if (str[i] == '(' || str[i] == '[')
-//		{
-//			st.push(str[i]);
-//		}
-//
-//		else if (str[i] == ')' && !st.empty() && st.top() == '(')
-//		{
-//			st.pop();
-//			if (str[i - 1] == '(')
-//				answer += 2;
-//			else
-//				answer *= 2;
-//		}
-//		else if (str[i] == ']' && !st.empty() && st.top() == '[')
-//		{
-//			st.pop();
-//			if (str[i - 1] == '[')
-//				answer += 3;
-//			else
-//				answer *= 3;
-//		}
-//		//0
-//		else if (str[i] == ')' && st.top() != '(')
-//		{
-//			cout << 0 << "\n";
-//			return 0;
-//		}
-//		else if (str[i] == ']' && st.top() != '[')
-//		{
-//			cout << 0 << "\n";
-//			return 0;
-//		}
-//	}
-//	cout << answer;
-//}
-
-#include <iostream>
+#include <iostream> 
 #include <stack>
 #include <string>
 using namespace std;
@@ -69,47 +16,68 @@ int main()
 	for (int i = 0; i < str.size(); ++i)
 	{
 		int tempVal = 0;
+
 		if (str[i] == '(' || str[i] == '[')
 			st.push(str[i]);
+
 		else if (str[i] == ')' && !st.empty() && st.top() == '(')
 		{
 			st.pop();
-			tempVal += 2;
-			stack<char> temp = st;
-			while (!temp.empty())
+			if (str[i - 1] == '(')
 			{
-				if (temp.top() == '(')
-					tempVal *= 2;
-				else if(temp.top() == '[')
-					tempVal *= 3;
-				temp.pop();
+				tempVal += 2;
+				stack<char> temp = st;
+				while (!temp.empty())
+				{
+					if (temp.top() == '(')
+						tempVal *= 2;
+					else if (temp.top() == '[')
+						tempVal *= 3;
+					temp.pop();
+				}
 			}
 		}
 		else if (str[i] == ']' && !st.empty() && st.top() == '[')
 		{
 			st.pop();
-			tempVal += 3;
-			stack<char> temp = st;
-			while (!temp.empty())
+			if (str[i - 1] == '[')
 			{
-				if (temp.top() == '(')
-					tempVal *= 2;
-				else if(temp.top() == '[')
-					tempVal *= 3;
-				temp.pop();
+				tempVal += 3;
+				stack<char> temp = st;
+				while (!temp.empty())
+				{
+					if (temp.top() == '(')
+						tempVal *= 2;
+					else if (temp.top() == '[')
+						tempVal *= 3;
+					temp.pop();
+				}
 			}
 		}
-		/*else if (str[i] == ')' && st.top() != '(')
+
+		//
+		else if (str[i] == ')')
 		{
-			cout << 0 << "\n";
-			return 0;
+			if (st.empty() || st.top() != '(')
+			{
+				cout << 0 << "\n";
+				return 0;
+			}
 		}
-		else if (str[i] == ']' && st.top() != '[')
+		else if (str[i] == ']')
 		{
-			cout << 0 << "\n";
-			return 0;
-		}*/		
+			if (st.empty() || st.top() != '[')
+			{
+				cout << 0 << "\n";
+				return 0;
+			}
+		}
 		answer += tempVal;
+	}
+	if (!st.empty())
+	{
+		cout << "0\n";
+		return 0;
 	}
 	cout << answer;
 }
