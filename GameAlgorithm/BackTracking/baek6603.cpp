d528a4bef2da3ce2213cfe2f1de686a70a0a7631
GB_LEEM <juntrex@naver.com>
2024-03-14 14:02:36 +0900

    [ADD] barking dog - lecture back tracking

--- GameAlgorithm/BackTracking/baek6603.cpp
+++ GameAlgorithm/BackTracking/baek6603.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int s=1, k;
+int s, k;
 int arr[100];
 int num[100];
 bool isused[100];
 
-void func(int cur)
+void func(int cur, int index)
 {
-	if (cur == s)
+	if (cur == 6)
 	{
-		for (int i = 0; i < s; ++i)
-			cout << num[arr[i]-1] << " ";
+		for (int i = 0; i < 6; ++i)
+			cout << arr[i] << " ";
 		cout << "\n";
 		return;
-	}
-
-	int index = 0;
-	if (cur != 0)
-	{
-		index = arr[cur - 1] + 1;
 	}
 	for (int i = index; i < s; ++i)
 	{
 		if (!isused[i])
 		{
-			arr[cur] = i;
+			arr[cur] = num[i]; 
 			isused[i] = 1;
-			func(cur + 1);
+			func(cur + 1, i + 1);
 			isused[i] = 0;
 		}
 	}
@@ -38,9 +32,14 @@ int main()
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	
-	cin >> s;
-	fill(num, num + s, 0);
-	for (int i = 0; i < s; ++i)
-		cin >> num[i];
-	func(0);
+	s = 1;
+	while (s != 0)
+	{
+		cin >> s;
+		fill(num, num + s, 0);
+		for (int i = 0; i < s; ++i)
+			cin >> num[i];
+		func(0, 0);
+		cout << "\n";
+	}
 }
