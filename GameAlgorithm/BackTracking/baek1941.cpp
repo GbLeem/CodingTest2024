cc422704fe4008840a387e62687e1b44419878e5
GB_LEEM <juntrex@naver.com>
2024-03-14 14:37:09 +0800

    [TEMP]

---
+++ GameAlgorithm/BackTracking/baek1941.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//
+//string board[5];
+//bool isused1[5];
+//bool isused2[5];
+//bool isused3[5];
+//int cnt = 0;
+//
+//void func(int cur)
+//{
+//	if (cur == 7)
+//	{
+//		cnt++;
+//		return;
+//	}
+//	for (int i = 0; i < 5; ++i)
+//	{
+//
+//	}
+//}
+//
+//int main()
+//{
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	for (int i = 0; i < 5; ++i)
+//		cin >> board[i]; //s y
+//
+//	
+//}
---
+++ GameAlgorithm/Simulation/baek15683.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int n, m; //세로 가로
+int board[10][10];
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			cin >> board[i][j];
+		}
+	}
+	
+}
