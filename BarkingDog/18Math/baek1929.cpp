6ffc8e96ba26ec3c7ea51957ed866f3111f5aee8
GB_LEEM <juntrex@naver.com>
2024-03-23 15:18:43 +0900

    [ADD] barking dog - lecture math

--- BarkingDog/18Math/baek1929.cpp
+++ BarkingDog/18Math/baek1929.cpp
@@ -9,12 +9,12 @@ vector<bool>state(1000001, true);
 
 void sieve(int n)
 {
-	state[1] = false;
-	for (int i = 2; i * i <= n; ++i)
+	state[1] = false; // 1 소수아님
+	for (int i = 2; i * i <= n; ++i) //i가 소수인지 판별
 	{
-		if (!state[i])
+		if (state[i] == false)
 			continue;
-		for (int j = i * i; j <= n; j += i)
+		for (int j = i * i; j <= n; j += i) //배수 거르기
 			state[j] = false;
 	}
 }
