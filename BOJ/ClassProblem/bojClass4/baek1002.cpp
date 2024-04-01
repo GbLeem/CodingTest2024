1e799028daa95222ef68ba321072ad00e7413e7f
GB_LEEM <juntrex@naver.com>
2024-04-01 14:48:09 +0900

    [ADD] silver3 1002

--- BOJ/ClassProblem/bojClass4/baek1002.cpp
+++ BOJ/ClassProblem/bojClass4/baek1002.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int t;
-int arr[10];
 int main()
 {
 	ios::sync_with_stdio(0);
@@ -11,7 +11,37 @@ int main()
 	cin >> t;
 	while (t--)
 	{
-		for (int i = 0; i < 6; ++i)
-			cin >> arr1[i];
+		int x1, y1, r1, x2, y2, r2;
+		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+
+		if (x1 == x2 && y1 == y2 && r1 == r2) //같음
+		{
+			if (r1 == 0)
+				cout << 0 << "\n";
+			else
+				cout << -1 << "\n";
+			continue;
+		}
+		
+		else if (x1 == x2 && y1 == y2 && r1 != r2) //중심 공유
+		{
+			cout << 0 << "\n";
+			continue;
+		}
+		else
+		{
+			double maxR = (r1 > r2) ? r1 : r2;
+			double minR = (r1 < r2) ? r1 : r2;
+
+			double dist = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+			dist = sqrt(dist);
+
+			if (dist == double(r2 + r1) || dist + minR == maxR) //접점 하나
+				cout << 1 << "\n";
+			else if (dist < double(r1 + r2) && dist + minR > maxR)
+				cout << 2 << "\n";
+			else
+				cout << 0 << "\n";
+		}
 	}
 }
