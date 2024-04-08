82018be173ffba29a0d8b363b11f14bae0d4245f
DESKTOP-FU7FP8I\user <a01085357914@gmail.com>
2024-04-08 23:43:30 +0900

    [ADD] codility lesson 6

--- CodingTest_Codility/Lesson6/Distinct.cpp
+++ CodingTest_Codility/Lesson6/Distinct.cpp
@@ -1,34 +1,21 @@
-#include <algorithm>
 #include <vector>
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 int solution(vector<int>& A)
 {
-    if (A.empty())
-        return 0;
-    sort(A.begin(), A.end()); // 1 1 1 2 2 3 3 100
-    vector<int> temp(A[A.size()-1]); // 0 0 0 0 0 0   
-
-    for (int i = 0; i < A.size(); ++i)
-    {
-        temp[A[i] - 1]++; //3 2 1 0 0 0
-    }
-    for (int i = 0; i < temp.size(); ++i)
-    {
-        if (temp[i] == 1)
-            return i + 1;
-    }
-    return 0;
+    sort(A.begin(), A.end());
+    A.erase(unique(A.begin(), A.end()), A.end());
+    return A.size();
 }
 
 int main()
 {
-    vector<int> a = { 2, 1, 1, 2, 3, 1,3, 100 };
-    vector<int> b = {  };
-    vector<int> c = { 10000000 };
+    vector<int> a = { 2, 1, 1, 2, 3, 1, 3 }; 
+    //vector<int> b = {  };
+    //vector<int> c = { 1, 1'000'000 };
     cout << solution(a) << endl;
-    cout << solution(b) << endl;
-    cout << solution(c) << endl;
+    //cout << solution(b) << endl;
+    //cout << solution(c) << endl;
 }
