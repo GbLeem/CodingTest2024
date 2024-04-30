2ec3d7b56d47e3273c2ef6a672f432f760d27d6a
GB_LEEM <juntrex@naver.com>
2024-04-30 13:37:04 +0900

    [ADD] barking dog - lecture binary search tree

---
+++ BarkingDog/22BinarySearchTree/baek23326.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+int n, q;
+set<int> s;
+int c = 1;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> q;
+	for (int i = 1; i <= n; ++i)
+	{
+		int num;
+		cin >> num;
+
+		if (num == 1)		
+			s.insert(i); // 2 5		
+	}
+
+	while (q--)
+	{
+		int query;
+		cin >> query;
+
+		if (query == 1)
+		{
+			int num;
+			cin >> num;
+
+			if (s.find(num) != s.end())//찾으면
+				s.erase(num);
+			else
+				s.insert(num);	
+		}
+		else if (query == 2)
+		{
+			int num;
+			cin >> num;
+
+			c = (c + num - 1) % n + 1;
+		}
+		else //3
+		{			
+			int ans = 0;
+			if (s.empty())
+				cout << -1 << "\n";
+			else
+			{
+				auto it = s.lower_bound(c);
+
+				if (it == s.end())
+					cout << n - c + *s.begin() << "\n";
+				else
+					cout << *it - c << "\n";
+			}
+		}
+	}
+}
