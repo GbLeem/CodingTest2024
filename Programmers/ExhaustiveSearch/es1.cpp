56b4a24893aab87e7739aafa2487fb5392715740
DESKTOP-FU7FP8I\user <a01085357914@gmail.com>
2024-05-06 23:17:45 +0900

    [ADD] programmers - lecture 완전탐색

--- Programmers/ExhaustiveSearch/es1.cpp
+++ Programmers/ExhaustiveSearch/es1.cpp
@@ -1,21 +1,21 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-int w = max(sizes[0][0], sizes[0][1]);
-int h = min(sizes[0][0], sizes[0][1]);
-
-pair<int, int> fit(int v1, int v2)
-{
-
-}
-
-int solution(vector<vector<int>> sizes)
-{
-    int answer = 0;
-
-
-
-    return answer;
-}
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//int w = max(sizes[0][0], sizes[0][1]);
+//int h = min(sizes[0][0], sizes[0][1]);
+//
+//pair<int, int> fit(int v1, int v2)
+//{
+//
+//}
+//
+//int solution(vector<vector<int>> sizes)
+//{
+//    int answer = 0;
+//
+//
+//
+//    return answer;
+//}
---
+++ Programmers/ExhaustiveSearch/es2.cpp
@@ -0,0 +1,64 @@
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool cmp(pair<int, int>a, pair<int, int>b)
+{
+    if (a.first == b.first)
+        return a.first > b.first;
+    return a.first > b.first;
+}
+vector<int> solution(vector<int> answers)
+{
+    vector<int> ans;
+    vector<pair<int, int>> p;
+    vector<int> one = { 1, 2, 3, 4, 5 };
+    vector<int> two = { 2, 1, 2, 3, 2, 4, 2, 5 };
+    vector<int> three = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+    
+    //나머지 연산
+    int oneA = 0;
+    int twoA = 0;
+    int threeA = 0;
+
+    for (int i = 0; i < answers.size(); ++i)
+    {
+        if (answers[i] == one[i % one.size()])
+            oneA++;
+        if (answers[i] == two[i % two.size()])
+            twoA++;
+        if (answers[i] == three[i % three.size()])
+            threeA++;
+    }
+    p.push_back({ oneA, 1 });
+    p.push_back({ twoA, 2 });
+    p.push_back({ threeA, 3 });
+    
+    sort(p.begin(), p.end(), cmp);
+
+    if (p[0].first == p[1].first)
+    {
+        if (p[1].first == p[2].first)
+        {
+            ans.push_back(1);
+            ans.push_back(2);
+            ans.push_back(3);
+        }
+        else
+        {
+            ans.push_back(p[0].second);
+            ans.push_back(p[1].second);
+        }
+    }
+    else
+        ans.push_back(p[0].second);
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> answer = { 1,2,3,4,5 };
+    solution(answer);
+}
