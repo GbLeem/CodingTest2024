7295f73a3b8b5e0796ea4cbbc9585cee1ffba09c
GB_LEEM <juntrex@naver.com>
2024-05-18 18:31:28 +0900

    [TEMP]

---
+++ CodingTest_Codility/Exercise1/2015contest2.cpp
@@ -0,0 +1,58 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+#include <iostream>
+
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int>& A)
+{    
+    int depth = 0;
+    priority_queue<int>pq;
+    priority_queue<int, vector<int>, greater<int>> pq2;
+    bool ok = false;
+
+    for (int i = 0; i < A.size(); ++i)
+    {
+        int tempDepth = 0;
+
+
+        if (ok && A[i-1] < A[i])
+        {
+            int minVal = pq2.top();
+            int maxVal = pq.top();
+            int Wall = min(A[i], maxVal);
+            tempDepth = Wall - minVal;
+
+            if (tempDepth > depth)
+                depth = tempDepth;
+            ok = false;
+            continue;
+        }        
+
+        pq.push(A[i]);
+        pq2.push(A[i]);
+        
+        if (pq.top() > A[i])
+        {
+            ok = true;
+        }             
+    }
+    return depth;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+   //vector<int> v = { 1,3,2,1,2,1,5,3,3,4,2 };
+    vector<int> v2 = { 5,8 };
+    //cout << solution(v) << "\n";
+    cout << solution(v2);
+}
