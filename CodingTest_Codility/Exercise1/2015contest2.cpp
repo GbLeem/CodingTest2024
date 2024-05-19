f9ab4c91a48356bb02d7a76e13983f2389190528
GB_LEEM <juntrex@naver.com>
2024-05-19 17:02:43 +0900

    [ADD] codility exercise1 - 2015 contest 2

--- CodingTest_Codility/Exercise1/2015contest2.cpp
+++ CodingTest_Codility/Exercise1/2015contest2.cpp
@@ -10,38 +10,66 @@
 
 using namespace std;
 
+//int solution(vector<int>& A)
+//{    
+//    int depth = 0;
+//    priority_queue<int>pq;
+//    priority_queue<int, vector<int>, greater<int>> pq2;
+//    bool ok = false;
+//
+//    for (int i = 0; i < A.size(); ++i)
+//    {
+//        int tempDepth = 0;
+//
+//
+//        if (ok && A[i - 1] < A[i])
+//        {
+//            int minVal = pq2.top();
+//            int maxVal = pq.top();
+//            int Wall = min(A[i], maxVal);
+//            tempDepth = Wall - minVal;
+//
+//            if (tempDepth > depth)
+//                depth = tempDepth;
+//            ok = false;
+//            continue;
+//        }        
+//
+//        pq.push(A[i]);
+//        pq2.push(A[i]);
+//        
+//        if (pq.top() > A[i])
+//        {
+//            ok = true;
+//        }             
+//    }
+//    return depth;
+//}
+
 int solution(vector<int>& A)
-{    
+{
+    if (A.size() <= 2)
+        return 0;
+
+    int tall = 0;
+    int small = 0;
     int depth = 0;
-    priority_queue<int>pq;
-    priority_queue<int, vector<int>, greater<int>> pq2;
-    bool ok = false;
-
-    for (int i = 0; i < A.size(); ++i)
+    int tempDepth = 0;
+    for (int i = 1; i < A.size(); ++i)
     {
-        int tempDepth = 0;
-
-
-        if (ok && A[i-1] < A[i])
+        if (A[i] > A[tall])
         {
-            int minVal = pq2.top();
-            int maxVal = pq.top();
-            int Wall = min(A[i], maxVal);
-            tempDepth = Wall - minVal;
+            tempDepth = A[tall] - A[small];
+            tall = i;
+            small = tall;
+        }
+        else if (A[i] > A[small])
+            tempDepth = A[i] - A[small];
+        else if (A[i] < A[small])
+            small = i;
 
-            if (tempDepth > depth)
-                depth = tempDepth;
-            ok = false;
-            continue;
-        }        
-
-        pq.push(A[i]);
-        pq2.push(A[i]);
-        
-        if (pq.top() > A[i])
-        {
-            ok = true;
-        }             
+        if (tempDepth > depth)
+            depth = tempDepth;
     }
     return depth;
 }
@@ -51,8 +79,11 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-   //vector<int> v = { 1,3,2,1,2,1,5,3,3,4,2 };
-    vector<int> v2 = { 5,8 };
-    //cout << solution(v) << "\n";
-    cout << solution(v2);
+    vector<int> v = { 1,3,2,1,2,1,5,3,3,4,2 };
+    vector<int> v2 = { 5, 8 };
+    vector<int> v3 = { 2, 1, 3 };
+
+    cout << solution(v) << "\n";  //2
+    cout << solution(v2) << "\n"; //0
+    cout << solution(v3) << "\n"; //1
 }
