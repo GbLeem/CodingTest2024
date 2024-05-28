dfa39e99ab3838357dd4ff88be03721d99a79026
GB_LEEM <juntrex@naver.com>
2024-05-28 18:42:09 +0900

    [TEMP] class 4

---
+++ BOJ/ClassProblem/bojClass4/baek1167.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int n;
+//vector<pair<int, int>> adj[100005];
+//
+//int main()
+//{
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	cin >> n;
+//
+//	for (int i = 0; i < n; ++i)
+//	{
+//		int u, v, l;
+//
+//		while (u != -1)
+//		{
+//			cin >> u >> v >> l;
+//
+//
+//		}
+//	}
+//}
