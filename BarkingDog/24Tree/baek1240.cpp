12a66a0cd17749f234448eafa9eabf48585d0c3a
GB_LEEM <juntrex@naver.com>
2024-05-28 18:32:57 +0900

    [ADD] barking dog - lecture tree

--- BarkingDog/24Tree/baek1240.cpp
+++ BarkingDog/24Tree/baek1240.cpp
@@ -1,68 +1,55 @@
-//#include <iostream>
-//#include <queue>
-//#include <vector>
-//using namespace std;
-//
-//int n, m;
-//int a[1005][1005];
-//bool vis[1005];
-//
-//int main()
-//{
-//	ios::sync_with_stdio(0);
-//	cin.tie(0);
-//
-//	cin >> n >> m;
-//
-//	for (int i = 0; i < n - 1; ++i)
-//	{
-//		int u, v, w;
-//		cin >> u >> v >> w;
-//		a[u][v] = w;
-//		a[v][u] = w;
-//	}
-//
-//	for (int i = 0; i < m; ++i)
-//	{
-//		int st, en;
-//		cin >> st >> en; // 1 2
-//
-//		int distance = 0;
-//		bool ok = false;
-//		fill(vis, vis + 1005, false);
-//		queue<int> q;
-//		q.push(st);
-//		vis[st] = true;
-//
-//		while (!q.empty())
-//		{
-//			auto cur = q.front();
-//			q.pop();			
-//
-//			if (cur == en || ok)
-//			{
-//				cout << distance << "\n";
-//				ok = false;
-//				break;
-//			}
-//
-//			for (int nxt = 1; nxt <= n; ++nxt)
-//			{				
-//				if (vis[nxt])
-//					continue;
-//				if (a[cur][nxt] > 0)
-//				{
-//					vis[nxt] = true;
-//					q.push(nxt);
-//					distance += a[cur][nxt];
-//
-//					if (nxt == en)
-//					{
-//						ok = true;
-//						break;
-//					}
-//				}
-//;			}
-//		}
-//	}
-//}
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int n, m;
+vector<pair<int, int>> adj[1005];
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n >> m;
+
+	for (int i = 0; i < n - 1; ++i)
+	{
+		int u, v, l;
+		cin >> u >> v >> l;
+
+		adj[u].push_back({ v, l }); 
+		adj[v].push_back({ u, l });
+	}
+	 
+	for (int i = 0; i < m; ++i)
+	{
+		int u, v;
+		cin >> u >> v;
+
+		queue<pair<int, int>> q;
+		vector<bool> vis(n + 2);
+
+		q.push({ u, 0 });
+		vis[u] = true;
+
+		while (!q.empty())
+		{
+			auto cur = q.front();
+			q.pop();
+			if (cur.first == v)
+			{
+				cout << cur.second << "\n";
+				break;
+			}
+			for (auto nxt : adj[cur.first])
+			{
+				if (vis[nxt.first])
+					continue;
+				vis[nxt.first] = true;
+				q.push({ nxt.first, cur.second + nxt.second });
+			}
+		}
+	}
+
+}
