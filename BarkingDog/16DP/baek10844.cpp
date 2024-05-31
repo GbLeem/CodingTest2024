e9caa9d6913605f842ece3a3977158fd2031060f
GB_LEEM <juntrex@naver.com>
2024-05-31 15:31:21 +0900

    [ADD] barking dog - lecture DP

--- BarkingDog/16DP/baek10844.cpp
+++ BarkingDog/16DP/baek10844.cpp
@@ -1,24 +1,34 @@
-//#include <iostream>
-//using namespace std;
-//
-//int n;
-//long long d[105];
-//
-//int main()
-//{
-//	ios::sync_with_stdio(0);
-//	cin.tie(0);
-//	
-//	cin >> n;
-//	//int endSize = 10 * n - 1;
-//	//int startSize = pow(10, n - 1);
-//
-//	d[1] = 9;
-//	
-//	for (int i = 2; i <= n; ++i)
-//	{
-//		d[i] = d[i - 1] * 2 - (i - 1);
-//	}
-//
-//	cout << d[n];
-//}
+#include <iostream>
+using namespace std;
+
+int n;
+long long d[105][10];
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n;
+	
+	for (int i = 1; i <= 9; ++i)
+		d[1][i] = 1;
+
+	//0으로 끝나거나 9로 끝나는 수 갯수 빼야함
+	for (int i = 2; i <= n; ++i)
+	{
+		for (int k = 0; k <= 9; ++k) //뒷자릿수
+		{
+			if (k != 0)
+				d[i][k] += d[i - 1][k - 1];
+			if (k != 9)
+				d[i][k] += d[i - 1][k + 1];
+			d[i][k] %= 1'000'000'000;
+		}
+	}
+	long long ans = 0;
+	for (int i = 0; i <= 9; ++i)
+		ans += d[n][i];
+	ans %= 1'000'000'000;
+	cout << ans;
+}
