dca8b85b71c30cf05e813371f47286a868118657
GB_LEEM <juntrex@naver.com>
2024-06-28 14:34:47 +0900

    [ADD] nypc 2023 - round1 exerciese1

--- NYPC/2023/round1_ex1.cpp
+++ NYPC/2023/round1_ex1.cpp
@@ -1,3 +1,80 @@
 //인류의 적 모기 퇴치
 
 #include <iostream>
+using namespace std;
+
+int n, m;
+int board[55][55];
+int dx[4] = { -1, 1,-1,1 };
+int dy[4] = { 1,1,-1,-1 };
+
+int dx1[4] = { 1,0,-1,0 };
+int dy1[4] = { 0,1,0,-1 };
+
+int CountCross(int i, int j)
+{
+	int cnt = board[i][j];
+	int nextValue[2] = { 0,0 };
+
+	for (int dir = 1; dir <= m; ++dir)
+	{
+		for (int nxt = 0; nxt < 4; ++nxt)
+		{
+			nextValue[0] = i + dir * dx1[nxt];
+			nextValue[1] = j + dir * dy1[nxt];
+
+			if (nextValue[0] < 0 || nextValue[0] > n || nextValue[1] < 0 || nextValue[1] > n)
+				continue;
+			cnt += board[nextValue[0]][nextValue[1]];
+		}
+
+	}
+	return cnt;
+}
+
+int CountX(int i, int j)
+{
+	int cnt = board[i][j];
+	int nextValue[2] = { 0,0 };
+
+	for (int dir = 1; dir <= m; ++dir)
+	{
+		for (int nxt = 0; nxt < 4; ++nxt)
+		{
+			nextValue[0] = i + dir * dx[nxt];
+			nextValue[1] = j + dir * dy[nxt];
+
+			if (nextValue[0] < 0 || nextValue[0] > n || nextValue[1] < 0 || nextValue[1] > n)
+				continue;
+			cnt += board[nextValue[0]][nextValue[1]];
+		}
+
+	}
+	return cnt;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n >> m;
+
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+			cin >> board[i][j];
+			
+	int maxValue = -1;
+	
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			if (maxValue < CountCross(i, j))
+				maxValue = CountCross(i, j);
+			if (maxValue < CountX(i, j))
+				maxValue = CountX(i, j);
+		}
+	}
+	cout << maxValue;
+}
