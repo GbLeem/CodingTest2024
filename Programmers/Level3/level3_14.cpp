7025fdec4c2800eadc721cae5cd4e785e345e663
GB_LEEM <juntrex@naver.com>
2024-07-09 16:08:05 +0900

    [TEMP] testcase 1 -> aborted core dumped

--- Programmers/Level3/level3_14.cpp
+++ Programmers/Level3/level3_14.cpp
@@ -8,8 +8,19 @@
 #include <algorithm>
 using namespace std;
 
+struct pair_hash
+{   
+    template<class T1, class T2>
+    size_t operator() (const pair<T1, T2>& pair) const
+    {
+        auto hash1 = hash<T1>{}(pair.first);
+        auto hash2 = hash<T2>{}(pair.second);
+        return hash1 ^ hash2;
+    }
+};
+
 unordered_multimap<string, string> um;
-unordered_map<string, bool> vis;
+unordered_map<pair<string, string>, int, pair_hash> vis;
 
 vector<string> solution(vector<vector<string>> tickets)
 {
@@ -18,38 +29,79 @@ vector<string> solution(vector<vector<string>> tickets)
     for (int i = 0; i < tickets.size(); ++i)
     {
         um.insert({ tickets[i][0], tickets[i][1] });
+        vis[{tickets[i][0], tickets[i][1]}]++; //중복된 티켓을 쓰기 위한 처리
     }
+    unordered_map<pair<string, string>, int, pair_hash> tempvis(vis);
 
     queue<string> q;
     q.push("ICN");
-    vis["ICN"] = 1;
     answer.push_back("ICN");
-
+    string before = "ICN";
+    string other = "A";
+    
     while (!q.empty())
-    {
+    {        
         auto cur = q.front(); //key값
         q.pop();
 
-        auto u = um.equal_range(cur);
+        auto u = um.equal_range(cur); //key값에 해당하는 value들
 
         vector<string> temp;
         for (auto it = u.first; it != u.second; ++it)
-        {
-            cout << it->second << " ";
-            if(vis[it->second] == 0)
+        {            
+            //방문하지 않은 경우(즉 티켓이 남아있는 경우)
+            if (vis[{before, it->second}] > 0)
+            {
                 temp.push_back(it->second);
+            }
         }
+
+        //갈 곳이 있다면,
         if (!temp.empty())
         {
-            sort(temp.begin(), temp.end());
-            q.push(temp[0]);
-            answer.push_back(temp[0]);        
-            vis[temp[0]] = 1;
-        }
-        
-        
+            int idx = 0;
+            sort(temp.begin(), temp.end()); //ATL, SFO            
+            
+            if(temp[idx] == other)
+            {
+                while (1)
+                {
+                    idx++;                                
+                    if (temp[idx] != other)
+                    {
+                        other = "";
+                        break;;
+                    }
+                }
+            }
+            q.push(temp[idx]);
+            vis[{before, temp[idx]}]--; //티켓 하나 지우기
+            answer.push_back(temp[idx]);
+            before = temp[idx];            
+        }              
 
-    }
+        //갈 곳이 없고, 다 방문하지 못한 경우
+        else if (temp.empty() && answer.size() < tickets.size())
+        {
+            //새로운 ICN 찾아서 시작
+            for (int i = 0; i < answer.size(); ++i)
+            {              
+                if (answer[i] == "ICN")
+                {
+                    //잘못된 경로 찾기                 
+                    other = answer[i + 1];
+                    break;
+                }                
+            }
+            answer.clear();
+            vis.clear();        
+            vis = tempvis; 
+
+            q.push("ICN");
+            before = "ICN";
+            answer.push_back("ICN");
+        }
+    }       
 
     return answer;
 }
@@ -58,7 +110,13 @@ int main()
 {
     vector<vector<string>> tickets = { {"ICN", "JFK"} ,{"HND", "IAD"},{"JFK", "HND"} };
     vector<vector<string>> tickets2 = { {"ICN", "SFO" }, { "ICN", "ATL" }, { "SFO", "ATL" }, { "ATL", "ICN" }, { "ATL","SFO" } };
-    auto vec = solution(tickets2);
+    vector<vector<string>> tickets3 = { {"ICN", "BBB"}, {"BBB", "ICN"}, {"ICN","AAA"} };
+    vector<vector<string>> tickets4 = { {"ICN", "AAA"}, {"AAA","ICN"},{"ICN","CCC"},{"CCC","ICN"},{"ICN","DDD"},{"DDD","AAA"} };
+    vector<vector<string>> tickets5 = { {"ICN", "JFK"} ,{"ICN", "AAD"},{"JFK", "ICN"} };
+    vector<vector<string>> tickets6 = { {"ICN", "AAA"} ,{"AAA", "ICN"},{"ICN", "CCC"},{"ICN", "DDD"},{"DDD", "ICN"} };
+    vector<vector<string>> tickets7 = { {"ICN", "AAA"}, {"ICN","AAA"},{"AAA","ICN"} };
+
+    auto vec = solution(tickets6);
 
     for (auto v : vec)
         cout << v << " ";
