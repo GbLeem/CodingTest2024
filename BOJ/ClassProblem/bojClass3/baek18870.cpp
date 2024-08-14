e62f17870286558ea522b4b8e3e22364e0d93f7e
GB_LEEM <juntrex@naver.com>
2024-08-15 01:27:34 +0900

    [ADD] class 3

--- BOJ/ClassProblem/bojClass3/baek18870.cpp
+++ BOJ/ClassProblem/bojClass3/baek18870.cpp
@@ -1,36 +1,31 @@
-//#include <iostream>
-//#include <vector>
-//#include <set>
-//#include <algorithm>
-//using namespace std;
-//
-//int main()
-//{
-//	ios::sync_with_stdio(0);
-//	cin.tie(0);
-//
-//	int N;
-//	cin >> N;
-//	vector<int> vec(N);
-//	for (int i = 0; i < N; ++i)
-//	{
-//		cin >> vec[i];
-//	}	
-//	vector<int> temp(vec); // 2 4 -10 4 -9
-//
-//	sort(vec.begin(), vec.end()); // -10 -9 2 4 4
-//
-//	set<int> s;
-//	for (int i = 0; i < vec.size(); ++i)
-//	{
-//		s.insert(vec[i]); //-10 -9 2 4
-//	}
-//	//0 1 2 3
-//	int index = s.size() - 1;
-//
-//	for (int i = 0; i < temp.size(); ++i)
-//	{
-//		
-//	}
-//
-//}
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+int arr[1000005];
+vector<int> vec;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> arr[i];
+		vec.push_back(arr[i]);
+	}
+	
+	sort(vec.begin(), vec.end()); //-10 -9 2 4 4 
+
+	vec.erase(unique(vec.begin(), vec.end()), vec.end());
+	for (int i = 0; i < n; ++i)
+	{
+		//찾고자 하는 것보다 같거나 큰 처음으로 발견된 위치(index)
+		cout << lower_bound(vec.begin(), vec.end(), arr[i]) - vec.begin() << " ";
+	}
+}
